90ec12e1c5b6bc07c228e6a83ab45ca19bf12ddd
243884919@qq.com <243884919@qq.com>
2017-02-07 16:58:07 +0800

    add grid

---
+++ MeshBaker_Test/Assets/FanZhengYong/Scripts/CamerForTargetInCenter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamerForTargetInCenter : MonoBehaviour 
+{
+    public Vector3  targetPos;
+
+    private Vector3 originCameraPos;
+    private Vector3 normalOriginCameraPos;
+    private Vector3 normalToTargetPos;
+	void Start () 
+    {
+        originCameraPos = transform.position;
+        normalOriginCameraPos = Vector3.Normalize(originCameraPos);
+        normalToTargetPos = Vector3.Normalize(originCameraPos - targetPos);
+        float angle = Vector3.Dot(normalToTargetPos, normalOriginCameraPos);
+        Debug.Log("angle=" + angle);
+        Debug.Log(Vector3.Angle(normalOriginCameraPos, normalToTargetPos));
+
+	}
+	
+
+}
