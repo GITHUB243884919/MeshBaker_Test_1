4bbecf73a9f34a1f5afba54606ce894454aa4ebd
243884919@QQ.COM <243884919@QQ.COM>
2017-02-19 01:28:09 +0800

    调整ai为按速度移动

---
+++ MeshBaker_Test/Assets/RA/Scrips/BattleLogic/AI/BattleAIArrive2.cs
@@ -0,0 +1,94 @@
+﻿/// <summary>
+/// AI行为类：到达指定目标
+/// autor: fanzhengyong
+/// date: 2017-02-16 
+/// </summary>
+using UnityEngine;
+using System.Collections;
+
+public class BattleAIArrive2 : BattleAISteering 
+{
+	private bool  m_isPlanar         = true;
+    
+    //减速区
+	public float m_slowDownDistance  = 2.0f;
+    
+    //到达区，距离多少算到了，可以设置为0;
+    public float m_nearDistance      = 0.5f;
+
+    //目标位置
+	public Vector3 m_target;
+
+    //预期速度
+	private Vector3 m_desiredVelocity;
+
+    //最大速度
+    private float m_maxSpeed;
+
+    //容器
+    private BattleAISteerings2 m_vehicle;
+
+    public delegate void DelegateArrived();
+    public DelegateArrived m_arrivedCallback = null;
+
+    private float m_uSpeed = 2.0f;
+    private Vector3 m_uForce = Vector3.zero;
+
+	void Start () 
+    {
+        Debug.Log("begin" + Time.realtimeSinceStartup);
+        Init();
+	}
+
+    public override void Init()
+    {
+        m_vehicle  = GetComponent<BattleAISteerings2>();
+        m_maxSpeed = m_vehicle.m_maxSpeed;
+        m_isPlanar = m_vehicle.m_isPlanar;
+
+        Vector3 toTarget = m_target - transform.position;
+        m_uForce = toTarget.normalized * m_uSpeed;
+        transform.LookAt(m_target);
+    }
+	
+	public override Vector3 Force()
+	{
+        //Debug.Log("Force");
+        Vector3 force = Vector3.zero;
+        if (m_target == null)
+        {
+            return force;
+        }
+
+		Vector3 toTarget = m_target - transform.position;
+		Vector3 desiredVelocity;
+        
+        if (m_isPlanar)
+        {
+            toTarget.y = 0;
+        }
+		float distance = toTarget.magnitude;
+        Debug.Log("distance " + distance + "near " + m_nearDistance);
+        force = m_uForce;
+        //到了
+        if (distance <= m_nearDistance)
+        {
+            //m_vehicle.m_velocity = Vector3.zero;
+            force = Vector3.zero;
+            Debug.Log("end" + Time.realtimeSinceStartup);
+
+            if (m_arrivedCallback != null)
+            {
+                m_arrivedCallback();
+            }
+        }
+
+        return force;
+	}
+
+    void OnDrawGizmos()
+    {
+        //Gizmos.color = Color.red;
+        //Gizmos.DrawWireSphere(target, slowDownDistance);
+    }
+}
---
+++ MeshBaker_Test/Assets/RA/Scrips/BattleLogic/AI/BattleAISteerings2.cs
@@ -0,0 +1,102 @@
+﻿/// <summary>
+/// AI行为容器
+/// 负责计算出容器内有效的（可能是多个）行为类形成的合力，最终对其形成作用
+/// autor: fanzhengyong
+/// date: 2017-02-16 
+/// </summary>
+using UnityEngine;
+using System.Collections;
+
+public class BattleAISteerings2: MonoBehaviour 
+{
+    private BattleAISteering[] m_steerings;
+    public float              m_maxSpeed = 10;
+    public float              m_maxForce = 100;
+    protected float           m_sqrMaxSpeed;
+    public float              m_mass = 1;
+    public Vector3            m_velocity;
+    public float              m_damping = 0.9f;
+    public float              m_computeInterval = 0.2f;
+    public bool               m_isPlanar = true;
+
+	private Vector3           m_steeringForce;
+    private Vector3           m_acceleration;
+	private float             m_timer = 0.0f;
+
+    public bool               m_displayTrack = true;
+    private Vector3           m_moveDistance;
+
+	protected void Start () 
+	{
+        m_timer           = 0.0f;
+
+        m_moveDistance   = Vector3.zero;
+        m_steeringForce  = Vector3.zero;
+        m_sqrMaxSpeed    = m_maxSpeed * m_maxSpeed;
+
+        m_steerings      = GetComponents<BattleAISteering>();
+	}
+
+	void Update () 
+	{
+		m_timer         += Time.deltaTime;
+		m_steeringForce  = Vector3.zero;
+
+        if (m_timer < m_computeInterval)
+        {
+            return;
+        }
+
+        Profiler.BeginSample("begin calc all force");
+
+        for (int i = 0; i < m_steerings.Length; i++)
+        {
+            if (m_steerings[i].enabled)
+            {
+                m_steeringForce += m_steerings[i].Force() * m_steerings[i].m_weight;
+            }
+        }
+
+        //m_steeringForce = Vector3.ClampMagnitude(m_steeringForce, m_maxForce);
+		m_acceleration  = m_steeringForce / m_mass;
+		m_timer         = 0;
+		
+        Profiler.EndSample();
+	}
+
+    void FixedUpdate()
+    {
+        //m_velocity += m_acceleration * Time.fixedDeltaTime;
+
+        //if (m_velocity.sqrMagnitude > m_sqrMaxSpeed)
+        //{
+        //    m_velocity = m_velocity.normalized * m_maxSpeed;
+        //}
+
+        //m_moveDistance = m_velocity * Time.fixedDeltaTime;
+
+        //if (m_isPlanar)
+        //{
+        //    m_velocity.y = 0;
+        //    m_moveDistance.y = 0;
+        //}
+
+        //if (m_displayTrack)
+        //{
+        //    Debug.DrawLine(transform.position,
+        //        transform.position + m_moveDistance, Color.red, 30.0f);
+        //}
+
+        m_moveDistance = m_acceleration * Time.fixedDeltaTime;
+        transform.position += m_moveDistance;
+
+        if (m_velocity.sqrMagnitude > 0.00001)
+        {
+            Vector3 newForward = Vector3.Slerp(transform.forward, m_velocity, m_damping * Time.deltaTime);
+            if (m_isPlanar)
+                newForward.y = 0;
+            transform.forward = newForward;
+        }
+
+    }
+}
