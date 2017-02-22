186d4a0e18712a65573cfdf711ef1ba6b441e194
243884919@qq.com <243884919@qq.com>
2017-02-22 12:24:40 +0800

    改用Commond模式控制坦克

---
+++ MeshBaker_Test/Assets/RA/Scrips/BattleLogic/CharController/CharController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharController : MonoBehaviour 
+{
+    public enum E_COMMOND
+    {
+        NONE,
+        IDLE,
+        ARRIVE,
+        ATTACK,
+        ATTACKED
+    }
+
+    public delegate void CommondCallback();
+
+    public Dictionary<E_COMMOND, CommondCallback> m_commondCallbacks =
+        new Dictionary<E_COMMOND, CommondCallback>();
+
+    TankCommond m_commond;
+    MoveSteers m_steers;
+    //date
+    public Vector3 TargetForArrive { get; set; }
+
+    public void Init()
+    {
+        m_commond = new TankCommond(this);
+        m_commond.Init();
+
+        m_steers = new MoveSteers(this);
+        m_steers.Init();
+
+        //到达行为统一处理
+        RegCommond(E_COMMOND.ARRIVE, Arrive);
+
+    }
+    public void RegCommond(E_COMMOND commond, CommondCallback callback)
+    {
+        CommondCallback _callback = null;
+        m_commondCallbacks.TryGetValue(commond, out _callback);
+        if (_callback != null)
+        {
+            Debug.LogWarning("CharController已存在Commond " + commond);
+            return;
+        }
+
+        m_commondCallbacks.Add(commond, callback);
+        
+    }
+
+    public void Commond(E_COMMOND commond)
+    {
+        CommondCallback callback = null;
+        m_commondCallbacks.TryGetValue(commond, out callback);
+        if (callback == null)
+        {
+            Debug.LogWarning("CharController不存在Commond " + commond);
+            return;
+        }
+        Debug.Log("Commond = " + commond + " " + TargetForArrive 
+            + " Time " + Time.realtimeSinceStartup);
+        callback();
+    }
+
+    public void Arrive()
+    {
+        MoveSteer arrive = m_steers.m_steers[MoveSteers.E_STEER_TYPE.ARRIVE];
+        arrive.Active = true;
+        m_steers.Active = true;
+    }
+	
+    //Unity
+	void Start () 
+    {
+        Init();
+	}
+	
+	void Update () 
+    {
+        m_commond.Update();
+        if (m_steers.Active)
+        {
+            m_steers.Update();
+        }
+        
+	}
+
+    void FixedUpdate()
+    {
+        if (m_steers.Active)
+        {
+            m_steers.FixedUpdate();
+        }
+    }
+}
---
+++ MeshBaker_Test/Assets/RA/Scrips/BattleLogic/CharController/Scene_Test_2.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Scene_Test_2 : MonoBehaviour
+{
+
+    void Start()
+    {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(BtnClick);
+    }
+
+
+
+    public void BtnClick()
+    {
+        Debug.Log("Begin click " + Time.realtimeSinceStartup);
+        StartCoroutine("CreateTanks");
+        //for (int i = 0; i < 128; i++ )
+        //{
+        //    //CreateTank(i, false, false);
+        //    CreateTank(i, true, false);
+
+        //}
+        Debug.Log("End click " + Time.realtimeSinceStartup);
+
+    }
+
+    IEnumerator CreateTanks()
+    {
+        yield return null;
+        for (int i = 0; i < 1; i++)
+        {
+            //CreateTank(i, false, false);
+            CreateTank(i, true, false);
+
+        }
+    }
+    void CreateTank(int entityID, bool withAI, bool withEffect)
+    {
+        BattleScene.E_BATTLE_OBJECT_TYPE type
+            = BattleScene.E_BATTLE_OBJECT_TYPE.TANK;
+        int serverEntityID = entityID;
+        int serverEntityType = 1;
+
+        Vector3 initPos = new Vector3(0f, 0f, 0f);
+
+        BattleModelObj obj = BattleScene.Instance.BorrowBattleModelObj(
+            type, serverEntityID, serverEntityType);
+
+        obj.m_go.transform.position = initPos;
+
+        //
+        if (withAI)
+        {
+
+            CharController ctr =  obj.m_go.GetComponent<CharController>();
+            Vector3 target = new Vector3(Random.Range(10, 310), 0f, Random.Range(10, 310));
+            ctr.TargetForArrive = target;
+            ctr.Commond(CharController.E_COMMOND.ARRIVE);
+        }
+
+        if (withEffect)
+        {
+            string effectPath = "Tank/Prefab/Tank_dapao_fire";
+            GameObject effectRes = Resources.Load<GameObject>(effectPath);
+            GameObject effectGo = GameObject.Instantiate<GameObject>(effectRes);
+            effectGo.transform.Rotate(new Vector3(0f, -90f, 0f));
+            Transform mount = obj.m_go.transform.Find("Bone01/Bone02/Dummy01");
+            effectGo.transform.SetParent(mount, false);
+        }
+
+    }
+
+}
